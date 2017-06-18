d95375dc304503f71bc9c4e693171aa737297514
Gurusewak Kalra <gurusewak.kalra@logmein.com>
2017-06-18 13:49:19 -0700

    Initial commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,7 @@
 import { combineReducers } from 'redux';
-import BooksReducer from './reducers_books';
-import ActiveBook from './reducers_active_book';
 
 const rootReducer = combineReducers({
-    books: BooksReducer,
-    activebook: ActiveBook
+
 });
 
 export default rootReducer;
